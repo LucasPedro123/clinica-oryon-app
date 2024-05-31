d9f1140fb22fd47b5f642ae78508a6f1ce81dd83
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-31 17:30:59 -0300

    feat: created navigation route

---
+++ src/Routes/stack.routes.tsx
@@ -0,0 +1,15 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import SignIn from '../Components/SignIn';
+import SignUp from '../Components/SignUp';
+
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function StackRoutes() {
+    return (
+        <Navigator>
+            <Screen name='signin' component={SignIn} options={{headerShown: false}}/>
+            <Screen name='signup' component={SignUp}/>
+        </Navigator>
+    )
+}
