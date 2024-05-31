6d99a01d90b656aff4b4f2e91747c3628c5c8590
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-31 17:29:50 -0300

    feat: created style guide definition

---
+++ src/Styles/global.tsx
@@ -0,0 +1,44 @@
+
+
+export const STYLE_GUIDE = {
+    Colors: {
+        primary: "#080A40",
+        secundary: "#836BA9",
+        highlight: "#1E2A7D",
+        complement: "background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #FFFFFF",
+        neutral: "#CCCCCC",
+        gray: "#828282",
+        emphasis: "#1A80B6",
+        aler: "#FF4D4D",
+        black: "#000000",
+        label: "#999EA1",
+        backgroundColor: "linear-gradient(240.86deg, #080A40 0%, #1A1B52 100%)"
+    },
+    BlueGradient: {
+        blue100: "#080A40",
+        blue200: "#1A1B52",
+        blue300: "#2B2C64",
+        blue400: "#3D3E76",
+        blue500: "#4E4F88",
+        blue600: "#60619A",
+        blue700: "#7172AC",
+        blue800: "#9495D0",
+        blue900: "#C8C4F2",
+    },
+    FontFamily: "'Manrope', sans-serif",
+    FontSize: {
+        headline: "34px",
+        subHead100: "25px",
+        subHead200: "18px",
+        subHead300: "16px",
+        subHead400: "14px",
+        description: "12px",
+        text: "10px"
+    },
+    FontWeight: {
+        regular: "400",
+        semiBold: "600",
+        bold: "700",
+    }
+
+}
