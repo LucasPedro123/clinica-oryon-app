cba76756b4605962aea1b4ce1469c6cf8bb1a62c
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-09 21:01:31 -0300

    feat: added statusbar

--- src/Routes/stack.routes.tsx
+++ src/Routes/stack.routes.tsx
@@ -2,16 +2,20 @@ import { createStackNavigator } from '@react-navigation/stack'
 import SignIn from '../Components/SignIn';
 import SignUp from '../Components/SignUp';
 import Home from '../Components/Home';
+import { StatusBar } from 'expo-status-bar';
 
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function StackRoutes() {
     return (
-        <Navigator>
-            <Screen name='signin' component={SignIn} options={{headerShown: false}}/>
-            <Screen name='signup' component={SignUp} options={{headerShown: false}}/>
-            <Screen name='home' component={Home} options={{headerShown: false}}/>
-        </Navigator>
+        <>
+            <StatusBar/>
+            <Navigator>
+                <Screen name='signin' component={SignIn} options={{ headerShown: false }} />
+                <Screen name='signup' component={SignUp} options={{ headerShown: false }} />
+                <Screen name='home' component={Home} options={{ headerShown: false }} />
+            </Navigator>
+        </>
     )
 }
