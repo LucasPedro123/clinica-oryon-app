704e4a3208147df6dcedebb5c1c676383df17743
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-13 23:38:04 -0300

    feat: added MyFoot component to Home

--- src/Components/MyFoot/index.tsx
+++ src/Components/MyFoot/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import * as S from './style'
 
-export default function MyFoot() {
-    return (    
-        <S.FootContainer>
-            <S.FootContent>
-                <S.MyFootTitle>
-                    Meus Alimentos
-                </S.MyFootTitle>
-                <S.TextCalories>
-                    530 Kcal
-                </S.TextCalories>
-            </S.FootContent>
-            <S.MyFootWrapper>
+import Feather from '@expo/vector-icons/Feather';
 
-            </S.MyFootWrapper>
+export default function MyFoot() {
+    function FootButton() {
+        return (
             <S.MyFootButton>
                 <S.ButtonText>
                     +
@@ -23,6 +15,33 @@ export default function MyFoot() {
                     Adicionar
                 </S.ButtonText>
             </S.MyFootButton>
+        )
+    }
+
+    return (
+        <S.FootContainer>
+            <S.FootContent>
+                <S.MyFootTitle>
+                    Meus Alimentos
+                </S.MyFootTitle>
+                <S.TextTotalCalories>
+                    530 Kcal
+                </S.TextTotalCalories>
+            </S.FootContent>
+            <S.MyFoot>
+                <S.MyFootWrapper>
+                    <S.MyFootView>
+                        <S.FootName>Pão de Mel</S.FootName>
+                        <S.FootCalories>30 cal</S.FootCalories>
+                    </S.MyFootView>
+                    <TouchableOpacity>
+                        <Feather name="trash-2" size={24} color="black" />
+                    </TouchableOpacity>
+                </S.MyFootWrapper>
+            </S.MyFoot>
+            <TouchableOpacity>
+                <FootButton />
+            </TouchableOpacity>
         </S.FootContainer>
     )
 }
