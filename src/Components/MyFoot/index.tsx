99c864aa88b9128789556dcd9dab23a9d1480a9a
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-22 01:36:31 -0300

    fix: fix: added data validation for query in the MyFoot component

--- src/Components/MyFoot/index.tsx
+++ src/Components/MyFoot/index.tsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import * as S from './style';
 import Feather from '@expo/vector-icons/Feather';
 import { UserContext } from '../../Context/User.context';
-import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs, Timestamp } from 'firebase/firestore';
 
 interface Nutrients {
     CHOCDF: number;
@@ -17,6 +17,7 @@ interface IFood {
     label: string;
     nutrients: Nutrients;
     foodId: string;
+    date: Timestamp;
 }
 
 const MyFoot = ({ navigation }: any) => {
@@ -24,23 +25,45 @@ const MyFoot = ({ navigation }: any) => {
     const [foodList, setFoodList] = useState<IFood[]>([]);
     const db = getFirestore();
 
+    const foodsCollectionRef = collection(db, `users/${context?.userId}/foods`);
+
     useEffect(() => {
         const fetchUserFoods = async () => {
             if (!context?.userId) return;
 
-            const foodsCollectionRef = collection(db, `users/${context.userId}/foods`);
+            const startOfDay = new Date();
+            startOfDay.setHours(0, 0, 0, 0);
+            const endOfDay = new Date();
+            endOfDay.setHours(23, 59, 59, 999);
+
+           
 
             try {
-                const querySnapshot = await getDocs(foodsCollectionRef);
-                const fetchedFoods = querySnapshot.docs.map(doc => ({ ...doc.data(), foodId: doc.id } as IFood));
+                const q = query(
+                    foodsCollectionRef,
+                    where('date', '>=', Timestamp.fromDate(startOfDay)),
+                    where('date', '<=', Timestamp.fromDate(endOfDay))
+                );
+                const querySnapshot = await getDocs(q);
+                const fetchedFoods = querySnapshot.docs.map(doc => {
+                    console.log('Doc Data:', doc.data());
+                    return {
+                        ...doc.data(),
+                        foodId: doc.id,
+                        date: doc.data().date.toDate()
+                    } as IFood;
+                });
+
+                console.log('Fetched foods:', fetchedFoods);
                 setFoodList(fetchedFoods);
             } catch (error) {
                 console.error('Erro ao buscar alimentos do usuário:', error);
+                Alert.alert('Erro', 'Ocorreu um erro ao buscar alimentos do usuário.');
             }
         };
 
         fetchUserFoods();
-    }, [context?.userId, foodList]);
+    }, [context?.userId, context?.newFood]);
 
     const totalCaloriesToday = foodList.reduce((total, food) => total + food.nutrients.ENERC_KCAL, 0).toFixed(2);
 
@@ -50,7 +73,7 @@ const MyFoot = ({ navigation }: any) => {
     };
 
     function handleNavigateForSearch() {
-        navigation.navigate('search')
+        navigation.navigate('search');
     }
 
     return (
@@ -59,21 +82,24 @@ const MyFoot = ({ navigation }: any) => {
                 <S.MyFootTitle>Meus Alimentos</S.MyFootTitle>
                 <S.TextTotalCalories>{totalCaloriesToday} Kcal</S.TextTotalCalories>
             </S.FootContent>
-            {foodList.map((food, index) => (
-                <S.MyFoot key={index}>
-                    
-                    <S.MyFootWrapper>
-                        <S.MyFootView>
-                            <S.FootName>{food.label}</S.FootName>
-                            <S.FootCalories>{`${food.nutrients.ENERC_KCAL.toFixed(0)} Kcal`}</S.FootCalories>
-                        </S.MyFootView>
-                        <TouchableOpacity onPress={() => handleRemoveFood(food.foodId)}>
-                            <Feather name="trash-2" size={24} color="black" />
-                        </TouchableOpacity>
-                    </S.MyFootWrapper>
-                </S.MyFoot>
-            ))}
-            <TouchableOpacity onPress={()=> {handleNavigateForSearch()}}>
+            {foodList.length > 0 ? (
+                foodList.map((food, index) => (
+                    <S.MyFoot key={index}>
+                        <S.MyFootWrapper>
+                            <S.MyFootView>
+                                <S.FootName>{food.label}</S.FootName>
+                                <S.FootCalories>{`${food.nutrients.ENERC_KCAL.toFixed(0)} Kcal`}</S.FootCalories>
+                            </S.MyFootView>
+                            <TouchableOpacity onPress={() => handleRemoveFood(food.foodId)}>
+                                <Feather name="trash-2" size={24} color="black" />
+                            </TouchableOpacity>
+                        </S.MyFootWrapper>
+                    </S.MyFoot>
+                ))
+            ) : (
+                <S.NoFoodMessage>Nenhum alimento adicionado hoje.</S.NoFoodMessage>
+            )}
+            <TouchableOpacity onPress={() => { handleNavigateForSearch(); }}>
                 <S.MyFootButton>
                     <S.ButtonText>+</S.ButtonText>
                     <S.ButtonText>Adicionar</S.ButtonText>
