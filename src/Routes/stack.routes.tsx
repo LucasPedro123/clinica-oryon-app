ef326b63c830e7033719909e760eecbb0be3edbe
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-23 22:22:32 -0300

    feat: added Forgot Pass to route

--- src/Routes/stack.routes.tsx
+++ src/Routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import Home from '../Screens/Home';
 import { StatusBar } from 'expo-status-bar';
 import { Profile } from '../Screens/Profile';
 import Search from '../Screens/Search';
+import { ForgotPass } from '../Screens/ForgotPass';
 
 
 const { Navigator, Screen } = createStackNavigator();
@@ -19,6 +20,7 @@ export default function StackRoutes() {
                 <Screen name='home' component={Home} options={{ headerShown: false }} />
                 <Screen name='profile' component={Profile} options={{ headerShown: false }} />
                 <Screen name='search' component={Search} options={{ headerShown: false }} />
+                <Screen name='forgotpass' component={ForgotPass} options={{ headerShown: true, headerTitle: '' }} />
             </Navigator>
         </>
     )
