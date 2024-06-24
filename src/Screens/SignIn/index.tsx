4ba14a805ac5372a5f6d35cd76ff4052af2b3e2d
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-23 22:33:25 -0300

    feat: added persistence for user authentication

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,36 +1,105 @@
-import { Text, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { Text, TouchableOpacity, Alert, ScrollView, ActivityIndicator } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import * as S from './style';
 import logo from '../../../assets/logoApp.png';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../Services/fireConfig';
 import GoogleLogo from '../../../assets/GoogleLogo.png';
 import { UserContext } from '../../Context/User.context';
+import { STYLE_GUIDE } from '../../Styles/global';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface AuthState {
+    id: string;
+    email: string;
+    pass: string;
+  }
 
 export default function SignIn({ navigation }: any) {
     const [checkIsVisible, setCheckIsVisible] = useState(false);
     const [passIsVisible, setPassIsVisible] = useState(true);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [loading, setLoading] = useState(false); // Estado para controlar o carregamento
     const context = useContext(UserContext);
 
+    useEffect(() => {
+        const checkPersistedAuth  = async () => {
+            try {
+                const authState = await AsyncStorage.getItem('persistedAuth');
+                
+                console.log(authState)
+                if (authState) {
+                    const jsonValue: AuthState = JSON.parse(authState);
+                    signInWithEmailAndPassword(auth, jsonValue.email, jsonValue.pass)
+                    .then((userCredential) => {
+                        const userId = userCredential.user.uid;
+                        context?.setUserId(jsonValue.id);
+                        navigation.navigate('home');
+                    })
+
+                }
+            } catch (error) {
+                console.error('Erro ao recuperar o estado de autenticação persistente:', error);
+            }
+        };
+        
+        checkPersistedAuth();
+        console.log(context?.userId)
+    }, []);
+    
+    
+
     const handleSignIn = () => {
+        setLoading(true); // Inicia o carregamento ao iniciar o login
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const userId = userCredential.user.uid;
-                console.log(userId); 
+                console.log(userId);
                 context?.setUserId(userId);
-                Alert.alert('Login bem-sucedido', 'Bem-vindo de volta!');
                 navigation.navigate('home');
+
+                const userInfo = {
+                    id: userId,
+                    email: email,
+                    pass: password
+                }
+
+                if (checkIsVisible) {
+                    
+                    AsyncStorage.setItem('persistedAuth', JSON.stringify(userInfo));
+                }
+
+                setEmailError(false);
+                setPasswordError(false);
             })
             .catch((error) => {
-                Alert.alert('Erro ao fazer login', error.message);
+                console.log("Error", error.code);
+                if (error.code === 'auth/invalid-email') {
+                    setEmailError(true);
+                    setPasswordError(false);
+                } else if (error.code === 'auth/wrong-password' || error.code === 'auth/missing-password' || error.code === 'auth/too-many-requests') {
+                    setEmailError(false);
+                    setPasswordError(true);
+                } else {
+                    setEmailError(true);
+                    setPasswordError(true);
+                }
+            })
+            .finally(() => {
+                setLoading(false); // Finaliza o carregamento após o login
             });
     };
 
     const handleNavigateForSignUp = () => {
         navigation.navigate('signup');
+    };
+
+    const handleNavigateForgotPass = () => {
+        navigation.navigate('forgotpass');
     };
 
     return (
@@ -52,11 +121,12 @@ export default function SignIn({ navigation }: any) {
                                 placeholder="Por favor, insira seu E-mail."
                                 value={email}
                                 onChangeText={setEmail}
+                                style={{ borderColor: emailError ? STYLE_GUIDE.Colors.alert : STYLE_GUIDE.Colors.borderColor }}
                             />
                         </S.InputWrapper>
                         <S.InputWrapper>
                             <S.InputName>Senha</S.InputName>
-                            <S.PasswordView>
+                            <S.PasswordView style={{ borderColor: passwordError ? STYLE_GUIDE.Colors.alert : STYLE_GUIDE.Colors.borderColor }}>
                                 <S.InputPassword
                                     placeholder="Por favor, insira sua senha."
                                     secureTextEntry={passIsVisible}
@@ -68,6 +138,7 @@ export default function SignIn({ navigation }: any) {
                                 </TouchableOpacity>
                             </S.PasswordView>
                         </S.InputWrapper>
+
                         <S.ForgotPassView>
                             <S.CheckBoxView>
                                 <S.CheckBox onPress={() => setCheckIsVisible(!checkIsVisible)}>
@@ -75,14 +146,21 @@ export default function SignIn({ navigation }: any) {
                                 </S.CheckBox>
                                 <S.CheckBoxText>Lembre-me</S.CheckBoxText>
                             </S.CheckBoxView>
-                            <TouchableOpacity>
+                            <TouchableOpacity onPress={handleNavigateForgotPass}>
                                 <S.ForgotPassText>Esqueceu a senha</S.ForgotPassText>
                             </TouchableOpacity>
                         </S.ForgotPassView>
                         <S.FormsButtonView>
-                            <S.FormsButton onPress={handleSignIn}>
-                                <S.ButtonText>Login</S.ButtonText>
-                            </S.FormsButton>
+                            {loading ? (
+                                <S.FormsButtonSpinner>
+                                    <ActivityIndicator size="small" color={STYLE_GUIDE.Colors.white} />
+                                </S.FormsButtonSpinner>
+
+                            ) : (
+                                <S.FormsButton onPress={handleSignIn}>
+                                    <S.ButtonText>Login</S.ButtonText>
+                                </S.FormsButton>
+                            )}
                             <S.DividerView>
                                 <S.Divider />
                                 <S.DividerText>Ou com</S.DividerText>
