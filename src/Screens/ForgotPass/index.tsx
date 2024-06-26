a115f7036143c8f1433e6733c4a20f1fa3554b9f
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-06-25 22:05:13 -0300

    feat: added email validation in the ForgotPass screen input

--- src/Screens/ForgotPass/index.tsx
+++ src/Screens/ForgotPass/index.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { Text, TouchableOpacity, Alert } from 'react-native';
-import { auth } from '../../Services/fireConfig'; // Certifique-se que este caminho está correto
-import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../Services/fireConfig';
+import { sendPasswordResetEmail, fetchSignInMethodsForEmail } from 'firebase/auth';
 import * as S from './style';
+import { STYLE_GUIDE } from '../../Styles/global';
 
 export function ForgotPass({ navigation }: any) {
     const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
 
     const handleResetPassword = async () => {
         try {
             await sendPasswordResetEmail(auth, email);
-            navigation.navigate('signin')
+            navigation.navigate('signin');
             Alert.alert('Sucesso', 'E-mail de redefinição de senha enviado!');
         } catch (error: any) {
-            Alert.alert('Erro', error.message);
+            setEmailError(true);
         }
     };
 
@@ -29,10 +31,11 @@ export function ForgotPass({ navigation }: any) {
                 <S.ForgotPassLabel >
                     Endereço de E-mail
                 </S.ForgotPassLabel>
-                <S.ForgotPassInput 
-                    placeholder='Insira seu E-mail' 
-                    value={email} 
-                    onChangeText={setEmail} 
+                <S.ForgotPassInput
+                    placeholder='Insira seu E-mail'
+                    value={email}
+                    onChangeText={setEmail}
+                    style={{ borderColor: emailError == true ? STYLE_GUIDE.Colors.alert : STYLE_GUIDE.Colors.borderColor }} // Adiciona a borda vermelha se houver erro de email
                 />
             </S.ForgotPassForm>
             <TouchableOpacity onPress={handleResetPassword}>
