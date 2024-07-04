e7ee38f1357d0651a9ab738357b77c05ae047fad
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-03 23:49:00 -0300

    fix: imported react-native-gesture-handler to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Routes from './src/Routes';
 
 export default function App() {
   return (
-    <Routes/>
+    <GestureHandlerRootView>
+      <Routes />
+    </GestureHandlerRootView>
   );
 }
-
