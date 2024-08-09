fdf8280ec4bda47f1de37cb92b70ec8a3f9438fc
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-08-08 23:32:56 -0300

    feat: created route type for CardItems component

---
+++ src/Routes/types.route.ts
@@ -0,0 +1,5 @@
+export type StackParamList = {
+    Home: undefined;
+    CardDetails: { image: any; title: string; description: string, paragraph: string };
+    CardAbout: { image: any; title: string; description: string, paragraph: string };
+  };
