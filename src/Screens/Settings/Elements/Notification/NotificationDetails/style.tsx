c1538655e7bae4656a10eb479659ae57b29a546d
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-08-23 23:02:53 -0300

    style: created notification details component

---
+++ src/Screens/Settings/Elements/Notification/NotificationDetails/style.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+import { STYLE_GUIDE } from '../../../../../Styles/global';
+
+export const Container = styled.View`
+    flex: 1;
+    width: 100%;
+    align-items: center;
+    margin-top: 10%;
+`
+
+
+export const Title = styled.Text`
+    font-weight: ${STYLE_GUIDE.FontWeight.semiBold};
+    font-size: ${STYLE_GUIDE.FontSize.subHead100};
+    line-height: 29px;
+    color: ${STYLE_GUIDE.Colors.primary};
+    margin-bottom: 36px;
+`
+
+export const NotificationTitle = styled.Text`
+    width: 320px;
+    font-family: 'Raleway';
+    font-weight: ${STYLE_GUIDE.FontWeight.semiBold};
+    font-size: 32px;
+    line-height: 44px;
+    color: #171923;
+`
+
+export const NotificationsDate = styled.Text`
+    font-weight: ${STYLE_GUIDE.FontWeight.regular};
+    font-size: ${STYLE_GUIDE.FontSize.subHead300};
+    line-height: 20px;
+    color: #171923;
+    opacity: 0.8;
+`
+
+export const NotificationDetailsContent = styled.View`
+    gap: 32px;
+    margin-bottom: 30px;
+`
+
+
+export const NotificationText = styled.Text`
+    width: 320px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #171923;
+    opacity: 0.5;
+`
