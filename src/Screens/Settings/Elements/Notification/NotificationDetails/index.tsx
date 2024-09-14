a11e32488d7cff1df908ba39c4b4ad71491321e3
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-09-13 23:23:50 -0300

    feat(Notifications): added notification read check

--- src/Screens/Settings/Elements/Notification/NotificationDetails/index.tsx
+++ src/Screens/Settings/Elements/Notification/NotificationDetails/index.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouteProp, useRoute } from '@react-navigation/native';
+import { ScrollView, TouchableOpacity, Linking } from 'react-native';
 import * as S from './style';
 import { Notification } from '../../../../../Interfaces/app.interfaces';
-import { formatDate } from '../../../../../utils/dateUtils'; // Ajuste o caminho conforme necessário
-import { ScrollView } from 'react-native';
+import { formatDate } from '../../../../../utils/dateUtils';
+import { markNotificationAsRead } from '../../../../../utils/notificationStorage';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { STYLE_GUIDE } from '../../../../../Styles/global';
 
-// Tipo de parâmetros esperados
 type NotificationDetailsRouteProp = RouteProp<{ NotificationDetails: { notification: Notification } }, 'NotificationDetails'>;
 
 export const NotificationDetails: React.FC = () => {
     const route = useRoute<NotificationDetailsRouteProp>();
     const { notification } = route.params;
 
+    useEffect(() => {
+        markNotificationAsRead(notification.id);
+    }, [notification.id]);
+
+    const handleOpenURL = (url: string) => {
+        Linking.openURL(url).catch((err) => console.error("Failed to open URL:", err));
+    };
+
     return (
-        <ScrollView
-            showsVerticalScrollIndicator={true}
-        >
+        <ScrollView showsVerticalScrollIndicator={true}>
             <S.Container>
                 <S.Title>Notificações</S.Title>
                 <S.NotificationDetailsContent>
                     <S.NotificationTitle>{notification.title}</S.NotificationTitle>
-                    <S.NotificationsDate>{formatDate(notification.date)} • Dr. Danilo Holfling</S.NotificationsDate>
-
-                    <S.NotificationText>
-                        {notification.content}
-                    </S.NotificationText>
+                    <S.Wrapper>
+                        <S.NotificationsDate>{formatDate(notification.date)} • Dr. Danilo Holfling</S.NotificationsDate>
+                        <S.ContentLinks>
+                            <TouchableOpacity onPress={() => handleOpenURL('https://www.instagram.com/drdanilohofling')}>
+                                <Entypo name="instagram" size={20} color={STYLE_GUIDE.Colors.gray200} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleOpenURL('https://www.linkedin.com/company/clinica-oryon/about/')}>
+                                <Feather name="linkedin" size={20} color={STYLE_GUIDE.Colors.gray200} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleOpenURL('https://wa.me/5511993516440')}>
+                                <FontAwesome name="whatsapp" size={20} color={STYLE_GUIDE.Colors.gray200} />
+                            </TouchableOpacity>
+                        </S.ContentLinks>
+                    </S.Wrapper>
+                    <S.NotificationText>{notification.content}</S.NotificationText>
                 </S.NotificationDetailsContent>
             </S.Container>
         </ScrollView>
--- src/Screens/Settings/Elements/Notification/index.tsx
+++ src/Screens/Settings/Elements/Notification/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { ScrollView, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
 import * as S from './style';
 import { useNotification } from '../../../../Context/Notifications.context';
-import { formatDate } from '../../../../utils/dateUtils'; // Ajuste o caminho conforme necessário
+import { formatDate } from '../../../../utils/dateUtils';
 import { STYLE_GUIDE } from '../../../../Styles/global';
+import { isNotificationRead } from '../../../../utils/notificationStorage'; // Importe as funções que criamos
+import { Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Notifications: React.FC = ({ navigation }: any) => {
     const { notifications, loading } = useNotification();
+    const [readStatuses, setReadStatuses] = useState<{ [key: string]: boolean }>({});
+
+    useEffect(() => {
+        const checkReadStatuses = async () => {
+            const statuses: { [key: string]: boolean } = {};
+            for (const notification of notifications) {
+                const isRead = await isNotificationRead(notification.id);
+                statuses[notification.id] = isRead;
+            }
+            setReadStatuses(statuses);
+        };
+        checkReadStatuses();
+    }, [notifications, handleNavigateNotificationDetails]);
 
     function handleNavigateNotificationDetails(notification: any) {
         navigation.navigate('NotificationDetails', { notification });
@@ -23,6 +39,7 @@ export const Notifications: React.FC = ({ navigation }: any) => {
                 {notifications.map((notification) => (
                     <S.Wrapper key={notification.id}>
                         <S.NotificationCard onPress={() => handleNavigateNotificationDetails(notification)}>
+                           {!readStatuses[notification.id] ? <S.BallRed /> : ''}
                             <S.NotificationData>{formatDate(notification.date)}</S.NotificationData>
                             <S.NotificationAuthor>Dr. Danilo Bianchini Höfling</S.NotificationAuthor>
                             <S.NotificationTitle>{notification.title}</S.NotificationTitle>
