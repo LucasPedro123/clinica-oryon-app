5a4710b6f2c086c2f6e8a0d325d5d46e71676cdb
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-09-16 22:30:49 -0300

    chore(Support): updated Accordion content

--- src/Screens/Settings/Elements/Support/index.tsx
+++ src/Screens/Settings/Elements/Support/index.tsx
@@ -11,7 +11,7 @@ export const SupportCenter: React.FC = () => {
             <S.Wrapper>
                 <AccordionElement icon='phone' title='Telefone' content='(11) 3073-0405'/>
                 <AccordionElement icon='whatsapp'title='Whatsapp' content='(11) 99351-6440'/>
-                <AccordionElement icon='email'title='Email' content='oryonestetica@clinicaoryon.com.br'/>
+                <AccordionElement icon='email'title='Email' content='oryon2@clinicaoryon.com.br'/>
                 <AccordionElement icon='web'title='Website' content='http://clinicaoryon.com.br'/>
             </S.Wrapper>
 
