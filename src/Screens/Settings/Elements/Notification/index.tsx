e9adfe055c490a3cf16de52bc357f9e2550b4061
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-12-13 01:31:53 -0300

    feat(Screen Notifications): added 'No notifications' text

--- src/Screens/Settings/Elements/Notification/index.tsx
+++ src/Screens/Settings/Elements/Notification/index.tsx
@@ -12,17 +12,17 @@ export const Notifications: React.FC = ({ navigation }: any) => {
     const { notifications, loading } = useNotification();
     const [readStatuses, setReadStatuses] = useState<{ [key: string]: boolean }>({});
 
-    useEffect(() => {
-        const checkReadStatuses = async () => {
-            const statuses: { [key: string]: boolean } = {};
-            for (const notification of notifications) {
-                const isRead = await isNotificationRead(notification.id);
-                statuses[notification.id] = isRead;
-            }
-            setReadStatuses(statuses);
-        };
-        checkReadStatuses();
-    }, [notifications, handleNavigateNotificationDetails]);
+   useEffect(() => {
+    const checkReadStatuses = async () => {
+        const statuses: { [key: string]: boolean } = {};
+        for (const notification of notifications) {
+            const isRead = await isNotificationRead(notification.id);
+            statuses[notification.id] = isRead;
+        }
+        setReadStatuses(statuses);
+    };
+    checkReadStatuses();
+}, [notifications, handleNavigateNotificationDetails]);
 
     function handleNavigateNotificationDetails(notification: any) {
         navigation.navigate('NotificationDetails', { notification });
@@ -36,23 +36,25 @@ export const Notifications: React.FC = ({ navigation }: any) => {
         <S.Container>
             <S.Title>Notificações</S.Title>
             <ScrollView>
-                {notifications.map((notification) => (
-                    <S.Wrapper key={notification.id}>
-                        <S.NotificationCard onPress={() => handleNavigateNotificationDetails(notification)}>
-                           {!readStatuses[notification.id] ? <S.BallRed /> : ''}
-                            <S.NotificationData>{formatDate(notification.date)}</S.NotificationData>
-                            <S.NotificationAuthor>Dr. Danilo Bianchini Höfling</S.NotificationAuthor>
-                            <S.NotificationTitle>{notification.title}</S.NotificationTitle>
-                            <S.NotificationContent>
-                                {notification.content.length > 50
-                                    ? `${notification.content.substring(0, 80)}...`
-                                    : `${notification.content}...`
-
-                                }
-                            </S.NotificationContent>
-                        </S.NotificationCard>
-                    </S.Wrapper>
-                ))}
+            {notifications.length === 0 ? (
+                    <Text style={{ textAlign: 'center', marginTop: 20 }}>Não há notificações</Text>
+                ) : (
+                    notifications.map((notification) => (
+                        <S.Wrapper key={notification.id}>
+                            <S.NotificationCard onPress={() => handleNavigateNotificationDetails(notification)}>
+                                {!readStatuses[notification.id] ? <S.BallRed /> : ''}
+                                <S.NotificationData>{formatDate(notification.date)}</S.NotificationData>
+                                <S.NotificationAuthor>Dr. Danilo Bianchini Höfling</S.NotificationAuthor>
+                                <S.NotificationTitle>{notification.title}</S.NotificationTitle>
+                                <S.NotificationContent>
+                                    {notification.content.length > 50
+                                        ? `${notification.content.substring(0, 80)}...`
+                                        : `${notification.content}...`}
+                                </S.NotificationContent>
+                            </S.NotificationCard>
+                        </S.Wrapper>
+                    ))
+                )}
             </ScrollView>
         </S.Container>
     );
