dada690993156226d981695b840a442d59146bac
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-03-09 22:49:44 -0300

    feat(contextFood: added food request by categories

--- src/Context/Foods.context.tsx
+++ src/Context/Foods.context.tsx
@@ -1,54 +1,99 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import axios from 'axios';
-import { Food, FoodContextType } from '../Interfaces/app.interfaces';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import axios from "axios";
+import { Food, FoodContextType } from "../Interfaces/app.interfaces";
 
 const FoodContext = createContext<FoodContextType | undefined>(undefined);
 
+const apiUrl = "https://food-data-json.vercel.app/api";
+
 const FoodProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [allFoodItems, setAllFoodItems] = useState<Food[]>([]);
-    const [foodItems, setFoodItems] = useState<Food[]>([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  const [categories, setCategories] = useState<string[]>([]);
+  const [foodItems, setFoodItems] = useState<Food[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string | undefined>("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const fetchFoodItems = async () => {
-        try {
-            const response = await axios.get('https://food-data-json-bm8g.vercel.app/api/foods');
-            const foods = response.data;
-            setAllFoodItems(foods);
-            setFoodItems(foods);
-            console.log(`Número de alimentos: ${foods.length}`);
-        } catch (error) {
-            console.error('Erro ao buscar alimentos: ', error);
-        }
-    };
 
-    useEffect(() => {
-        fetchFoodItems();
-    }, []);
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/categories`);
+      setCategories([...response.data]); // Forçar uma nova referência do array
+    } catch (error) {
+      console.error("Erro ao buscar categorias: ", error);
+    }
+  };
+  
+  const fetchFoodItems = async (category: string) => {
+    try {
+      const response = await axios.get(`${apiUrl}/categories/${category}`);
+      setFoodItems(response.data);
+    } catch (error) {
+      console.error("Error fetching food items:", error);
+    }
+  };
 
-    useEffect(() => {
-        if (searchTerm) {
-            const filteredFoods = allFoodItems.filter(food =>
-                food.name.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            setFoodItems(filteredFoods);
-        } else {
-            setFoodItems(allFoodItems);
-        }
-    }, [searchTerm, allFoodItems]);
+  const fetchFoodItemsByCategory = async (category: string) => {
+    try {
+      const response = await axios.get(`${apiUrl}/categories/${category}`);
+      setFoodItems(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar alimentos: ", error);
+    }
+  };
 
-    return (
-        <FoodContext.Provider value={{ foodItems, searchTerm, setSearchTerm, fetchFoodItems }}>
-            {children}
-        </FoodContext.Provider>
-    );
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    if (selectedCategory) {
+      fetchFoodItems(selectedCategory);
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    console.log(searchTerm)
+    if (searchTerm) {
+      const filteredFoods = foodItems.filter((food) =>
+        food.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFoodItems(filteredFoods);
+    } else if (selectedCategory) {
+      fetchFoodItemsByCategory(selectedCategory);
+    } else {
+      setFoodItems([]);
+    }
+  }, [searchTerm]);
+
+  return (
+    <FoodContext.Provider
+      value={{
+        categories,
+        foodItems,
+        searchTerm,
+        setSearchTerm,
+        selectedCategory,
+        setSelectedCategory,
+        fetchCategories,
+        fetchFoodItems
+      }}
+    >
+      {children}
+    </FoodContext.Provider>
+  );
 };
 
 const useFoodContext = () => {
-    const context = useContext(FoodContext);
-    if (!context) {
-        throw new Error('useFoodContext must be used within a FoodProvider');
-    }
-    return context;
+  const context = useContext(FoodContext);
+  if (!context) {
+    throw new Error("useFoodContext must be used within a FoodProvider");
+  }
+  return context;
 };
 
 export { FoodProvider, useFoodContext };
